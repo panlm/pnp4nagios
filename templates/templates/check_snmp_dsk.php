82765def52147cb87ccfde9967dde68b8d7e64a4
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-11-28 07:23:10 +0000

    check_snmp_dsk
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@38 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_dsk.php
+++ templates/templates/check_snmp_dsk.php
@@ -5,27 +5,38 @@
 #
 # Disk
 #
-$fsname = ereg_replace(".rrd","",ereg_replace(".*_","",$RRDFILE[1]));
-$ds_name[1] = $hostname . ":" . $fsname;
-$opt[1] = "--vertical-label DISK(MB) -l0  --title \"Disk Usage for fs:\"" . $fsname;
+$services = $this->tplGetServices($hostname,"check_snmp_dsk");
 
-$def[1] = rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
-$def[1] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
+$ds_name[3] = "";
+$opt[3]     = "";
+$def[3]     = "";
 
-$def[1] .= rrd::line2("var1", "#CCCC33", "Disksize") ;
-$def[1] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var2", "#33FFFF", "DiskUsed") ;
-$def[1] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+if ( ! preg_match("/(check_snmp_dsk_root|check_snmp_dsk_c)/",$servicedesc) ) {
+    return;
+}
 
-#
-# Disk Utilization
-#
-$ds_name[2] = $hostname . ":" . $fsname;
-$opt[2] = "--vertical-label DiskUtil(%) -l0  --title \"Disk Utilization for fs:\"" . $fsname;
+$ds_name[3] = $hostname . " File Systems";
+$opt[3]     = "--vertical-label \"Util(%)\" -l0 --title \"$ds_name[3] \" -u 100 ";
+$def[3]     = "";
+foreach($services as $key=>$val){
+    $a = $this->tplGetData($val['host'],$val['service']);
+    $def[3]    .= rrd::def("a$key" ,$a['DS'][2]['RRDFILE'], $a['DS'][2]['DS'], "AVERAGE");
+    #$def[3]    .= rrd::line2("a$key", rrd::color($key), $a['MACRO']['HOSTNAME'].'-'.$a['MACRO']['SERVICEDESC']);
+    $def[3]    .= rrd::line2("a$key", rrd::color($key+2), ereg_replace(".*_","",$a['MACRO']['SERVICEDESC']));
+    $def[3]    .= rrd::gprint("a$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+}
 
-$def[2] = rrd::def("var3", $RRDFILE[1], $DS[3], "AVERAGE");
-
-$def[2] .= rrd::line2("var3", "#336666", "DiskUtil") ;
-$def[2] .= rrd::gprint("var3", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$ds_name[4] = $hostname . " File Systems";
+$opt[4]     = "--vertical-label \" \" -l0 --title \"$ds_name[3] \" -u 100 --units-exponent=0 ";
+$def[4]     = "";
+foreach($services as $key=>$val){
+    $a = $this->tplGetData($val['host'],$val['service']);
+    $def[4]    .= rrd::def("a$key" ,$a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
+    $def[4]    .= rrd::def("b$key" ,$a['DS'][1]['RRDFILE'], $a['DS'][1]['DS'], "AVERAGE");
+    $def[4]    .= rrd::line2("a$key", rrd::color($key+2), ereg_replace(".*_","",$a['MACRO']['SERVICEDESC']).'-total');
+    $def[4]    .= rrd::gprint("a$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+    $def[4]    .= rrd::line2("b$key", rrd::color($key+2), ereg_replace(".*_","",$a['MACRO']['SERVICEDESC']).'-used');
+    $def[4]    .= rrd::gprint("b$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+}
 
 ?>
