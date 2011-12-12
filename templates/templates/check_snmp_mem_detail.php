5d69c1f9d30cf16934a4305f155a596583f75af6
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-12-12 13:24:04 +0000

    change style, units-exponent
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@80 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_mem_detail.php
+++ templates/templates/check_snmp_mem_detail.php
@@ -7,48 +7,60 @@ $ds_name[0] = "SWAP Usage";
 $opt[0]     = "";
 $def[0]     = "";
 
-$opt[0] .= "--vertical-label Size(KB) -l0  --title \"SWAP Usage for $hostname\" ";
+$opt[0] .= "--vertical-label \"Size(GB)\" -l0  --title \"SWAP Usage for $hostname\" --units-exponent=0 ";
 
 $def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
 $def[0] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
 
-$def[0] .= rrd::line2("var1", rrd::color(1), "TotalSwap") ;
-$def[0] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[0] .= rrd::line2("var2", rrd::color(2), "AvailSwap") ;
-$def[0] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[0] .= rrd::comment(" (NO STACKED GRAPH)\\r");
+$def[0] .= rrd::cdef("cvar1", "var1,1024,/,1024,/");
+$def[0] .= rrd::cdef("cvar2", "var2,1024,/,1024,/");
+$def[0] .= rrd::cdef("cvar3", "cvar1,cvar2,-");
+
+#$def[0] .= rrd::area("cvar1", rrd::color(1), "TotalSwap") ;
+#$def[0] .= rrd::gprint("cvar1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[0] .= rrd::area("cvar3", rrd::color(6), "Used") ;
+$def[0] .= rrd::gprint("cvar3", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[0] .= rrd::area("cvar2", rrd::color(4), "Avail", "STACK") ;
+$def[0] .= rrd::gprint("cvar2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[0] .= rrd::comment(" (STACKED GRAPH)\\r");
 
 #
 $ds_name[1] = "Memory Usage";
 $opt[1]     = "";
 $def[1]     = "";
 
-$opt[1] .= "--vertical-label Size(KB) -l0  --title \"Memory Usage for $hostname\" ";
+$opt[1] .= "--vertical-label Size(GB) -l0  --title \"Memory Usage for $hostname\" --units-exponent=0 ";
 
-$def[1] .= rrd::def("var3", $RRDFILE[1], $DS[3], "AVERAGE");
+#$def[1] .= rrd::def("var3", $RRDFILE[1], $DS[3], "AVERAGE");
 $def[1] .= rrd::def("var4", $RRDFILE[1], $DS[4], "AVERAGE");
 $def[1] .= rrd::def("var5", $RRDFILE[1], $DS[5], "AVERAGE");
 $def[1] .= rrd::def("var6", $RRDFILE[1], $DS[6], "AVERAGE");
 $def[1] .= rrd::def("var7", $RRDFILE[1], $DS[7], "AVERAGE");
 
-$def[1] .= rrd::line2("var3", rrd::color(3), "TotalMem") ;
-$def[1] .= rrd::gprint("var3", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var4", rrd::color(4), "AvailMem") ;
-$def[1] .= rrd::gprint("var4", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var5", rrd::color(5), "Shared") ;
-$def[1] .= rrd::gprint("var5", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var6", rrd::color(6), "Buffer") ;
-$def[1] .= rrd::gprint("var6", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var7", rrd::color(7), "Cached") ;
-$def[1] .= rrd::gprint("var7", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::comment(" (NO STACKED GRAPH)\\r");
+#$def[1] .= rrd::cdef("cvar3", "var3,1024,/,1024,/");
+$def[1] .= rrd::cdef("cvar4", "var4,1024,/,1024,/");
+$def[1] .= rrd::cdef("cvar5", "var5,1024,/,1024,/");
+$def[1] .= rrd::cdef("cvar6", "var6,1024,/,1024,/");
+$def[1] .= rrd::cdef("cvar7", "var7,1024,/,1024,/");
+
+#$def[1] .= rrd::area("cvar3", rrd::color(3), "TotalMem") ;
+#$def[1] .= rrd::gprint("cvar3", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[1] .= rrd::area("cvar5", rrd::color(1), "Shared") ;
+$def[1] .= rrd::gprint("cvar5", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[1] .= rrd::area("cvar6", rrd::color(6), "Buffer", "STACK") ;
+$def[1] .= rrd::gprint("cvar6", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[1] .= rrd::area("cvar7", rrd::color(2), "Cached", "STACK") ;
+$def[1] .= rrd::gprint("cvar7", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[1] .= rrd::area("cvar4", rrd::color(4), "Avail", "STACK") ;
+$def[1] .= rrd::gprint("cvar4", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+$def[1] .= rrd::comment(" (STACKED GRAPH)\\r");
 
 #
 $ds_name[2] = "Memory Utilization";
 $opt[2]     = "";
 $def[2]     = "";
 
-$opt[2] .= "--vertical-label Percent(%) -l0  --title \"Memory Utilization for $hostname\" ";
+$opt[2] .= "--vertical-label \"Percent(%)\" -l0  --title \"Memory Utilization for $hostname\" ";
 
 $def[2] .= rrd::def("var1", $RRDFILE[1], $DS[8], "AVERAGE");
 $def[2] .= rrd::def("var2", $RRDFILE[1], $DS[9], "AVERAGE");
