1953d607e42dc2b1a8d20373ab240e3720516375
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-12-13 03:22:54 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@81 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_dsk.php
+++ templates/templates/check_snmp_dsk.php
@@ -1,14 +1,14 @@
 <?php
 # EMPTY GRAPH
 if ( ! preg_match("/(check_snmp_dsk_root|check_snmp_dsk_c)/",$servicedesc) ) {
-    $ds_name[0] = "SEE GRAPH ABOVE";
-    $opt[0]     = "--vertical-label \" \" -l0 --title \" SEE GRAPH ABOVE \" -h 10 ";
+    $ds_name[0] = "This is a empty graph";
+    $opt[0]     = "--vertical-label \" \" -l0 --title \" This is a empty graph \" -h 10 ";
     $def[0]     = "";
     $def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
     $def[0] .= rrd::cdef("cvar1", "var1,0,*");
-    $def[0] .= rrd::line1("cvar1", rrd::color(2), "SEE GRAPH ABOVE") ;
+    $def[0] .= rrd::line1("cvar1", rrd::color(2), "This is a empty graph") ;
     $def[0] .= rrd::gprint("cvar1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-    $def[0] .= rrd::comment(" SEE GRAPH ABOVE \\r");
+    $def[0] .= rrd::comment(" This is a empty graph \\r");
     return;
 }
 
@@ -30,16 +30,18 @@ foreach($services as $key=>$val){
 }
 
 $ds_name[4] = $hostname . " File Systems";
-$opt[4]     = "--vertical-label \" \" -l0 --title \"$ds_name[3] \" -u 100 --units-exponent=0 ";
+$opt[4]     = "--vertical-label \" Size(GB) \" -l0 --title \"$ds_name[3] \" -u 100 --units-exponent=0 ";
 $def[4]     = "";
 foreach($services as $key=>$val){
     $a = $this->tplGetData($val['host'],$val['service']);
     $def[4]    .= rrd::def("a$key" ,$a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
     $def[4]    .= rrd::def("b$key" ,$a['DS'][1]['RRDFILE'], $a['DS'][1]['DS'], "AVERAGE");
-    $def[4]    .= rrd::line2("a$key", rrd::color($key+2), ereg_replace(".*_","",$a['MACRO']['SERVICEDESC']).'-total');
-    $def[4]    .= rrd::gprint("a$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
-    $def[4]    .= rrd::line1("b$key", rrd::color($key+2), ereg_replace(".*_","",$a['MACRO']['SERVICEDESC']).'-used');
-    $def[4]    .= rrd::gprint("b$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+    $def[4]    .= rrd::cdef("ca$key", "a$key,1024,/");
+    $def[4]    .= rrd::cdef("cb$key", "b$key,1024,/");
+    $def[4]    .= rrd::line2("ca$key", rrd::color($key+2), ereg_replace(".*_","",$a['MACRO']['SERVICEDESC']).'-total');
+    $def[4]    .= rrd::gprint("ca$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+    $def[4]    .= rrd::line1("cb$key", rrd::color($key+2), ereg_replace(".*_","",$a['MACRO']['SERVICEDESC']).'-used');
+    $def[4]    .= rrd::gprint("cb$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
 }
 
 ?>
