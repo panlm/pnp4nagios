065b031898489c04b7df634f5fbbfdc17a2e5f94
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-12-25 08:00:55 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@170 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_io.php
+++ templates/templates/check_snmp_io.php
@@ -12,6 +12,9 @@ $opt[0] .= "--vertical-label \"blocks\" -l0  --title \"$ds_name[0] for $hostname
 $def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
 $def[0] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
 
+#$def[0] .= rrd::cdef("var1", "cvar1,4,/");
+#$def[0] .= rrd::cdef("var2", "cvar2,4,/");
+
 $def[0] .= rrd::line2("var1", rrd::color(2), "IOSent") ;
 $def[0] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 $def[0] .= rrd::line2("var2", rrd::color(3), "IOReceive") ;
