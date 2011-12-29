939f9d813bd8cd86507d42dfe792cb8e5281a02a
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-12-29 09:07:18 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@195 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_storage.php
+++ templates/templates/check_snmp_storage.php
@@ -14,23 +14,19 @@ if ($MAX[1] != "") {
 
 if(preg_match("/memory/i", $NAME[1])) {
     $ds_name[0] = "Physical Memory";
-    $opt[0]    .= "--vertical-label \"(MB)\" --title \"$ds_name[0] ($MAX[1]MB) for $hostname\" --units-exponent=0 " . $myopt ;
+    $opt[0]    .= "--vertical-label \"(MB)\" -l0 --title \"$ds_name[0] ($MAX[1]MB) for $hostname\" --units-exponent=0 " . $myopt ;
     $def[0]    .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
-#    $def[0]    .= rrd::vdef("var2", "var1,AVERAGE");
     $def[0]    .= rrd::line2("var1", rrd::color(2), "Used") ;
     $def[0]    .= rrd::gprint("var1", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
-#    $def[0]    .= " GPRINT:var2:\"%6.2lf\" ";
     if ($WARN[1] != "") {
-        $def[0]    .= rrd::hrule($WARN[1], "#FF8c00", "Warning ");
+        $def[0]    .= rrd::hrule($WARN[1], "#FF8c00", "Warning ".$WARN[1].$UNIT[1]."\\n");
     }
     if ($CRIT[1] != "") {
         $def[0]    .= rrd::hrule($CRIT[1], "#FF0000", "Critial ");
     }
-#    $def[0]    .= rrd::comment("\\r");
-#    $def[0]    .= rrd::comment("Total\\: $MAX[1]MB\\l");
 } elseif ( preg_match("/^[CDEF_].*/", $NAME[1]) ) {
     $ds_name[0] = "Disk C";
-    $opt[0]    .= "--vertical-label \"(MB)\" --title \"$ds_name[0] ($MAX[1]MB) for $hostname\" --units-exponent=0 " . $myopt ;
+    $opt[0]    .= "--vertical-label \"(MB)\" -l0 --title \"$ds_name[0] ($MAX[1]MB) for $hostname\" --units-exponent=0 " . $myopt ;
     $def[0]    .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
     $def[0]    .= rrd::line2("var1", rrd::color(3), "Used") ;
     $def[0]    .= rrd::gprint("var1", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
@@ -41,7 +37,9 @@ if(preg_match("/memory/i", $NAME[1])) {
         $def[0]    .= rrd::hrule($CRIT[1], "#FF0000", "Critial ");
     }
 } else {
+
     include("/usr/local/groundwork/pnp/share/templates.dist/default.php");
+
 }
 
 ?>
