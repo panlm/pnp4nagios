f41d0bf263c2ea6d4f7cf76adecf5162588845c9
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-12-31 01:41:29 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@197 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_processor.php
+++ templates/templates/check_snmp_processor.php
@@ -14,35 +14,6 @@ $color_list = array(
     8 => "#336633"  // darker green
 );
 
-if(!preg_match('/^ProcessorLoad$/', $NAME[1])) {
-#graph 1
-$ds_name[0] = "Processor Load";
-$def[0] = "";
-$opt[0] = "--vertical-label \"%\" -l0  --title \"$ds_name[0] for $hostname\" ";
-for($i=1;$i<=count($DS);$i++){
-    $def[0] .= rrd::def("var$i", $RRDFILE[1], $DS[$i], "AVERAGE");
-    $def[0] .= rrd::cdef("cvar$i", "var$i,8,/");
-    $label = $i - 1;
-    $label = "cpu" . $label;
-#    $def[0] .= rrd::line1("cvar$i", $color_list[$i]."FF", "$label", "STACK") ;
-    $def[0] .= rrd::area("cvar$i", $color_list[$i]."ff", "$label", "STACK") ;
-    $def[0] .= rrd::gprint("var$i", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
-}
-#graph 1
-$ds_name[1] = "Processor Load";
-$def[1] = "";
-$opt[1] = "--vertical-label \"%\" -l0  --title \"$ds_name[0] for $hostname\" ";
-for($i=1;$i<=count($DS);$i++){
-    $def[1] .= rrd::def("cvar$i", $RRDFILE[1], $DS[$i], "AVERAGE");
-#    $def[1] .= rrd::cdef("cvar$i", "var$i,8,/");
-    $label = $i - 1;
-    $label = "cpu" . $label;
-    $def[1] .= rrd::line1("cvar$i", $color_list[$i]."FF", "$label") ;
-#    $def[1] .= rrd::area("cvar$i", $color_list[$i]."32", "$label") ;
-    $def[1] .= rrd::gprint("cvar$i", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
-}
-}
-
 if(preg_match('/^ProcessorLoad$/', $NAME[1])) {
     $ds_name[0] = "Processor Load";
     $def[0] = "";
@@ -50,6 +21,39 @@ if(preg_match('/^ProcessorLoad$/', $NAME[1])) {
     $def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
     $def[0] .= rrd::line2("var1", rrd::color(2), "average") ;
     $def[0] .= rrd::gprint("var1", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
+} elseif (preg_match('/^p[0-9]+/', $NAME[1])) {
+    #graph 1
+    $ds_name[0] = "Processor Load";
+    $def[0] = "";
+    $opt[0] = "--vertical-label \"%\" -l0  --title \"$ds_name[0] for $hostname\" ";
+    for($i=1;$i<=count($DS);$i++){
+        $def[0] .= rrd::def("var$i", $RRDFILE[1], $DS[$i], "AVERAGE");
+        $def[0] .= rrd::cdef("cvar$i", "var$i,8,/");
+        $label = $i - 1;
+        $label = "cpu" . $label;
+    #    $def[0] .= rrd::line1("cvar$i", $color_list[$i]."FF", "$label", "STACK") ;
+        $def[0] .= rrd::area("cvar$i", $color_list[$i]."ff", "$label", "STACK") ;
+        $def[0] .= rrd::gprint("var$i", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
+    }
+
+    #graph 2
+    $ds_name[1] = "Processor Load";
+    $def[1] = "";
+    $opt[1] = "--vertical-label \"%\" -l0  --title \"$ds_name[0] for $hostname\" ";
+    for($i=1;$i<=count($DS);$i++){
+        $def[1] .= rrd::def("cvar$i", $RRDFILE[1], $DS[$i], "AVERAGE");
+    #    $def[1] .= rrd::cdef("cvar$i", "var$i,8,/");
+        $label = $i - 1;
+        $label = "cpu" . $label;
+        $def[1] .= rrd::line1("cvar$i", $color_list[$i]."FF", "$label") ;
+    #    $def[1] .= rrd::area("cvar$i", $color_list[$i]."32", "$label") ;
+        $def[1] .= rrd::gprint("cvar$i", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
+    }
+} else {
+
+    include("/usr/local/groundwork/pnp/share/templates.dist/default.php");
+
 }
+
 
 ?>
