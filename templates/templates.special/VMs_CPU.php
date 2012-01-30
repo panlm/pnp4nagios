184bc31ce83a0bdbddc7710d483682f6851bbb81
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-01-30 06:29:34 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@248 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates.special/VMs_CPU.php
+++ templates/templates.special/VMs_CPU.php
@@ -1,16 +1,27 @@
 <?php
-#
+
+$color_list = array(
+    1 => "#ff77ee", // Purple
+    2 => "#fed409", // Yellow
+    3 => "#007dd0", // Blue
+    4 => "#ee0a04", // Red
+    5 => "#56a901", // Green
+    6 => "#ff6600", // Orange
+    7 => "#a4a4a4", // Grey
+    8 => "#336633"  // darker green
+);
+
 # Some Macros 
 $this->MACRO['TITLE'] = "All ISVs CPU Maximum Utilization Reports"; 
 $this->MACRO['COMMENT'] = " ";
 
-$services = $this->tplGetServices("ent|isv","check_snmp_cpu_");
+$services = $this->tplGetServices("local|ent|isv","check_snmp_cpu_|check_snmp_processor");
 
 $i = 0;
 foreach($services as $key=>$val){
     $a = $this->tplGetData($val['host'],$val['service']);
-    $ds_name[$i] = $a['MACRO']['HOSTNAME'];
-    $opt[$i]     = "--vertical-label \"Util(%)\" -l0 --title \" \" -u 100 -r ";
+    $ds_name[$i] = $a['MACRO']['HOSTNAME'] . ":" . $a['MACRO']['SERVICEDESC'];
+    $opt[$i]     = "--vertical-label \"Util(%)\" -l0 --title \" $ds_name[$i]: \" -u 100 -r ";
     $def[$i]     = "";
     if ( preg_match("/check_snmp_cpu_detail/",$val['service']) ) {
         $def[$i]    .= rrd::def("a$key" ,$a['DS'][1]['RRDFILE'], $a['DS'][1]['DS'], "AVERAGE");
@@ -32,6 +43,19 @@ foreach($services as $key=>$val){
         $def[$i]    .= rrd::def("b$key" ,$a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
         $def[$i]    .= rrd::area("b$key", "#FF0000", "cpu");
         $def[$i]    .= rrd::gprint("b$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+    } elseif ( preg_match("/check_snmp_processor$/",$val['service']) ) {
+        #$ds_name[$i] = "Processor Load";
+        #$def[$i] = "";
+        #$opt[$i] = "--vertical-label \"%\" -l0  --title \"$ds_name[0] for $hostname\" ";
+        $def[$i] .= rrd::def("var1", $a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
+        $def[$i] .= rrd::line2("var1", rrd::color(2), "average") ;
+        $def[$i] .= rrd::gprint("var1", array("MIN", "AVERAGE", "MAX"), "%6.2lf");
+    } else {
+        $def[$i] .= rrd::def("var1", $a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
+        $def[$i] .= rrd::cdef("cvar1", "var1,0,*");
+        $def[$i] .= rrd::line1("cvar1", rrd::color(2), "This is a empty graph") ;
+        $def[$i] .= rrd::gprint("cvar1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+        $def[$i] .= rrd::comment(" This is a empty graph \\r");
     }
     $i = $i + 1;
 }
