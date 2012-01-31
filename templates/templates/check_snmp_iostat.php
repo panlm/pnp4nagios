94a9e6457bcbd3953bd1877497aa08c3a1192089
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-01-31 01:47:57 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@250 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_iostat.php
+++ templates/templates/check_snmp_iostat.php
@@ -3,22 +3,24 @@
 # Copyright (c) 2006-2010 Joerg Linge (http://www.pnp4nagios.org)
 # Plugin: check_load
 #
-$ds_name[0] = "request merged";
-$opt[0]     = "";
-$def[0]     = "";
-
-$opt[0] .= "--vertical-label \"\" -l0  --title \"$ds_name[0] for $hostname\" --units-exponent=0 ";
-
-$def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
-$def[0] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
-
-$def[0] .= rrd::line2("var1", rrd::color(1), "rrqm/s") ;
-$def[0] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[0] .= rrd::line2("var2", rrd::color(2), "wrqm/s") ;
-$def[0] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[0] .= rrd::comment("\\r");
-$def[0] .= rrd::comment("The number of read/write requests merged per second that were queued to the\\l");
-$def[0] .= rrd::comment("device.\\l");
+# the following graph is always zero
+#
+# $ds_name[0] = "request merged";
+# $opt[0]     = "";
+# $def[0]     = "";
+# 
+# $opt[0] .= "--vertical-label \"\" -l0  --title \"$ds_name[0] for $hostname\" --units-exponent=0 ";
+# 
+# $def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
+# $def[0] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
+# 
+# $def[0] .= rrd::line2("var1", rrd::color(1), "rrqm/s") ;
+# $def[0] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+# $def[0] .= rrd::line2("var2", rrd::color(2), "wrqm/s") ;
+# $def[0] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
+# $def[0] .= rrd::comment("\\r");
+# $def[0] .= rrd::comment("The number of read/write requests merged per second that were queued to the\\l");
+# $def[0] .= rrd::comment("device.\\l");
 
 #
 $ds_name[1] = "read/write requests";
