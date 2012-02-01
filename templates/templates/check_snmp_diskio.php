dabab635edc4d37aa476018dd2575b406b10342e
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-02-01 09:06:20 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@251 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_diskio.php
+++ templates/templates/check_snmp_diskio.php
@@ -1,20 +1,19 @@
 <?php
 #
 # Copyright (c) 2006-2010 Joerg Linge (http://www.pnp4nagios.org)
-# Plugin: check_load
 #
 $ds_name[0] = "DiskIO";
 $opt[0]     = "";
 $def[0]     = "";
 
-$opt[0] .= "--vertical-label \"(KB)\" -l0  --title \"$ds_name[0] for $hostname\" --units-exponent=0 ";
+$opt[0] .= "--vertical-label \"requests\" -l0  --title \"$ds_name[0] for $hostname\" --units-exponent=0 ";
 
-$def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
-$def[0] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
+$def[0] .= rrd::def("var1", $RRDFILE[1], $DS[3], "AVERAGE");
+$def[0] .= rrd::def("var2", $RRDFILE[1], $DS[4], "AVERAGE");
 
-$def[0] .= rrd::line2("var1", rrd::color(2), "Read KB/s") ;
+$def[0] .= rrd::line2("var1", rrd::color(2), "Read/s") ;
 $def[0] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[0] .= rrd::line2("var2", rrd::color(3), "Write KB/s") ;
+$def[0] .= rrd::line2("var2", rrd::color(3), "Write/s") ;
 $def[0] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 
 #
@@ -22,14 +21,14 @@ $ds_name[1] = "DiskIO";
 $opt[1]     = "";
 $def[1]     = "";
 
-$opt[1] .= "--vertical-label \"requests\" -l0  --title \"$ds_name[1] for $hostname\" --units-exponent=0 ";
+$opt[1] .= "--vertical-label \"(KB)\" -l0  --title \"$ds_name[1] for $hostname\" --units-exponent=0 ";
 
-$def[1] .= rrd::def("var1", $RRDFILE[1], $DS[3], "AVERAGE");
-$def[1] .= rrd::def("var2", $RRDFILE[1], $DS[4], "AVERAGE");
+$def[1] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
+$def[1] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
 
-$def[1] .= rrd::line2("var1", rrd::color(2), "Read/s") ;
+$def[1] .= rrd::line2("var1", rrd::color(2), "Read KB/s") ;
 $def[1] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-$def[1] .= rrd::line2("var2", rrd::color(3), "Write/s") ;
+$def[1] .= rrd::line2("var2", rrd::color(3), "Write KB/s") ;
 $def[1] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 
 ?>
