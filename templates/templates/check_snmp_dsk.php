68d40a172313f98ebfb555754930dc9943139913
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-02-15 04:29:28 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@267 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates/check_snmp_dsk.php
+++ templates/templates/check_snmp_dsk.php
@@ -22,17 +22,17 @@ $def[0]     = "";
 #$def[3]    .= rrd::gprint("a$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
 
 $ds_name[0] = " File Systems: " . preg_replace('/check_snmp_dsk_/', '', $NAGIOS_SERVICEDESC);
-$num = 100 / ( $MAX[1] / 1024 ) ;
-$opt[0]     = " --vertical-label \" Size(GB) \" -l0 --title \"$ds_name[0] \" --units-exponent=0 ";
-$opt[0]    .= " --right-axis $num:0 --right-axis-label \"(%)\" ";
+$num = 100 / ( $MAX[1] * 1024 * 1024 ) ;
+$opt[0]     = " --vertical-label \" (bytes) \" -l0 --title \"$ds_name[0] \" ";
+$opt[0]    .= " --right-axis $num:0 --right-axis-label \"(%)\" --base 1024 ";
 $def[0]     = "";
 $def[0]    .= rrd::def("a$key" ,$RRDFILE[1], $DS[1], "AVERAGE");
 $def[0]    .= rrd::def("b$key" ,$RRDFILE[1], $DS[2], "AVERAGE");
-$def[0]    .= rrd::cdef("ca$key", "a$key,1024,/");
-$def[0]    .= rrd::cdef("cb$key", "b$key,1024,/");
-$def[0]    .= rrd::line1("ca$key", $color_list[4], 'Total');
-$def[0]    .= rrd::gprint("ca$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+$def[0]    .= rrd::cdef("ca$key", "a$key,1024,*,1024,*");
+$def[0]    .= rrd::cdef("cb$key", "b$key,1024,*,1024,*");
+#$def[0]    .= rrd::gprint("ca$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
 $def[0]    .= rrd::area("cb$key", $color_list[8], 'Used');
 $def[0]    .= rrd::gprint("cb$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+$def[0]    .= rrd::line1("ca$key", $color_list[4], 'Total '.$MAX[1]."G");
 
 ?>
