01e2a6593afa6c065634709daf0ba30181f73db4
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-02-29 02:23:54 +0000

    publish color_list array to stand alone php file
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@293 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates.special/vip-esx-cpu.php
+++ templates/templates.special/vip-esx-cpu.php
@@ -4,16 +4,7 @@
 #
 #
 
-$color_list = array(
-    1 => "#ff77ee", // Purple
-    2 => "#fed409", // Yellow
-    3 => "#007dd0", // Blue
-    4 => "#ee0a04", // Red
-    5 => "#56a901", // Green
-    6 => "#ff6600", // Orange
-    7 => "#336633", // darker green
-    8 => "#a4a4a4"  // Grey
-);
+require(dirname(__FILE__).'/../templates/color.php');
 
 $this->MACRO['TITLE'] = "CPU Utilization for ALL ESX Servers"; 
 $this->MACRO['COMMENT'] = " ";
