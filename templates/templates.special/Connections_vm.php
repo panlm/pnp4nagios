80fecacfc411f8816a84b52986cf94c03f17b29a
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-03-11 14:07:01 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@344 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates.special/Connections_vm.php
+++ templates/templates.special/Connections_vm.php
@@ -12,7 +12,7 @@ $services = $this->tplGetServices("vm","check_snmp_conn");
 #
 # The Name of this Datasource (ds)
 $ds_name[0] = "Connections"; 
-$opt[0]     = "--vertical-label \"Connections\" -l0 --title \" \" --units-exponent=0 --base=1000 ";
+$opt[0]     = "--vertical-label \"Connections\" -l0 --title \" \" --units-exponent=0 --base=1000 -h 800 ";
 $def[0]     = "";
 
 foreach($services as $key=>$val){
@@ -26,7 +26,7 @@ foreach($services as $key=>$val){
     #throw new Kohana_exception(print_r($a,TRUE));
     $def[0]    .= rrd::def("a$key" ,$a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
     $def[0]    .= rrd::line2("a$key", rrd::color($key), $a['MACRO']['HOSTNAME']);
-    $def[0]    .= rrd::gprint("a$key", array("MIN", "AVERAGE", "MAX"), "%.2lf%s");
+    $def[0]    .= rrd::gprint("a$key", array("LAST", "AVERAGE", "MAX"), "%.2lf%s");
 }
 
 ?>
