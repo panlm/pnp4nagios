354b8fe6f72ec3df09213edc4878ad11f22d2aef
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-04-06 11:11:39 +0000

    git-svn-id: https://172.29.254.10:3000/svn/projects/pnp4nagios/trunk@395 2e5cfa46-41e0-4c30-b113-11416295e233

--- templates/templates.special/vm_Conn.php
+++ templates/templates.special/vm_Conn.php
@@ -18,8 +18,19 @@ $def[0]     = "";
 foreach($services as $key=>$val){
     $a = $this->tplGetData($val['host'],$val['service']);
     $def[0]    .= rrd::def("a$key" ,$a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
-    $def[0]    .= rrd::line2("a$key", rrd::color($key%20), $a['MACRO']['HOSTNAME']);
+    $def[0]    .= rrd::line1("a$key", rrd::color($key%20), $a['MACRO']['HOSTNAME']);
     $def[0]    .= rrd::gprint("a$key", array("LAST", "AVERAGE", "MAX"), "%.2lf");
 }
+
+#$ds_name[1] = "Connections"; 
+#$opt[1]     = "--vertical-label \"Connections\" -l0 --title \" \" --units-exponent=0 --base=1000 -h 800 ";
+#$def[1]     = "";
+
+#foreach($services as $key=>$val){
+#    $a = $this->tplGetData($val['host'],$val['service']);
+#    $def[1]    .= rrd::def("a$key" ,$a['DS'][0]['RRDFILE'], $a['DS'][0]['DS'], "AVERAGE");
+#    $def[1]    .= rrd::area("a$key", rrd::color($key%20), $a['MACRO']['HOSTNAME'], "STACK");
+#    $def[1]    .= rrd::gprint("a$key", array("LAST", "AVERAGE", "MAX"), "%.2lf");
+#}
 
 ?>
