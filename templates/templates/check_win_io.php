88842040220009dc15de96971238487e19cfaf19
panlm <stevenpan@gmail.com>
2012-07-26 10:48:08 +0800

    update check_win_io.php

--- templates/templates/check_win_io.php
+++ templates/templates/check_win_io.php
@@ -7,7 +7,7 @@ $ds_name[0] = "Percent Idle/Busy Time";
 $opt[0]     = "";
 $def[0]     = "";
 
-$opt[0] .= "--vertical-label \"\" -l0  --title \"$ds_name[0] for $hostname\" -l 0 -u 100 --rigid ";
+$opt[0] .= "--vertical-label \" % \" -l0  --title \"$ds_name[0] for $hostname\" -l 0 -u 100 --rigid ";
 
 $def[0] .= rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE");
 $def[0] .= rrd::def("var2", $RRDFILE[1], $DS[2], "AVERAGE");
