9db5f6950dc101f90ce70b701d73ae91ebb40367
panlm <stevenpan@gmail.com>
2016-06-12 12:28:29 +0800

    add CMDs

--- github.cmd
+++ github.cmd
@@ -1,14 +1,19 @@
-cd /pub/git
-mkdir abc && cd abc
-git init .
-git pull git@github.com:panlm/pnp4nagios.git
+# git config --global user.name "panlm"
+# git config --global user.email stevenpan@gmail.com
+
+===
+
+#cd /pub/git
+#mkdir abc && cd abc
+#git init .
+#git pull git@github.com:panlm/pnp4nagios.git
 
 or 
 
-cd /pub/git
-git clone git@github.com:panlm/pnp4nagios.git
+#cd /pub/git
+#git clone git@github.com:panlm/pnp4nagios.git
 
-====
+===
 
 git remote add panlm git@github.com:panlm/pnp4nagios.git
 
