ca3874c6563970d82e7df49f952268ea5d93065d
root <root@gw2.(none)>
2016-06-12 12:25:57 +0800

    add some cmds

--- github.cmd
+++ github.cmd
@@ -1,3 +1,15 @@
+cd /pub/git
+mkdir abc && cd abc
+git init .
+git pull git@github.com:panlm/pnp4nagios.git
+
+or 
+
+cd /pub/git
+git clone git@github.com:panlm/pnp4nagios.git
+
+====
+
 git remote add panlm git@github.com:panlm/pnp4nagios.git
 
 git add filename
